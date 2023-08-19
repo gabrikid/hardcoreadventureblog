73b165b4bc4bf2c3db0253a2306e1ccd95c10eed
Gabriel Ferreira <gabrielsf7@gmail.com>
2023-08-19 15:31:40 +0100

    Loading spring boot project.

---
+++ src/test/java/pt/sardoalware/gabrikid/hardcoreadventureblog/HardcoreadventureblogApplicationTests.java
@@ -0,0 +1,13 @@
+package pt.sardoalware.gabrikid.hardcoreadventureblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HardcoreadventureblogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
