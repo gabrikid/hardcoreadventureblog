58b60d7a67d2043bf17666915fd501001bd35b09
Gabriel Ferreira <gabrielsf7@gmail.com>
2023-08-30 01:00:20 +0100

    Addition of missing unit tests related to AuthorController.

--- src/test/java/pt/sardoalware/gabrikid/hardcoreadventureblog/controller/AuthorControllerTest.java
+++ src/test/java/pt/sardoalware/gabrikid/hardcoreadventureblog/controller/AuthorControllerTest.java
@@ -7,18 +7,20 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import pt.sardoalware.gabrikid.hardcoreadventureblog.dto.AuthorDeleteResponseDto;
 import pt.sardoalware.gabrikid.hardcoreadventureblog.dto.AuthorRequestDto;
 import pt.sardoalware.gabrikid.hardcoreadventureblog.dto.AuthorResponseDto;
+import pt.sardoalware.gabrikid.hardcoreadventureblog.exception.AuthorNotFoundException;
+import pt.sardoalware.gabrikid.hardcoreadventureblog.exception.EmailAlreadyExistsException;
 import pt.sardoalware.gabrikid.hardcoreadventureblog.service.AuthorService;
-
 import java.util.List;
 
 public class AuthorControllerTest {
 
     private AuthorService authorServiceMock;
     private AuthorController authorController;
-    private AuthorRequestDto requestFirst, requestSecond;
-    private AuthorResponseDto responseFirst, responseSecond;
+    private AuthorRequestDto requestFirst;
+    private AuthorResponseDto responseFirst;
 
     @BeforeEach
     public void setupTest() {
@@ -27,22 +29,19 @@ public class AuthorControllerTest {
         authorController = new AuthorController(authorServiceMock);
 
         requestFirst = new AuthorRequestDto(NAME_TEST_1, EMAIL_TEST_1);
-        requestSecond = new AuthorRequestDto(NAME_TEST_2, EMAIL_TEST_2);
 
         responseFirst = new AuthorResponseDto(ID_TEST_1, NAME_TEST_1, EMAIL_TEST_1);
-        responseSecond = new AuthorResponseDto(ID_TEST_2, NAME_TEST_2, EMAIL_TEST_2);
     }
 
     @Test
     public void findAll() {
-        // don't need to validate AuthorResponseDto against a copy, as records are immutable!
-        when(authorServiceMock.findAll()).thenReturn(List.of(responseFirst, responseSecond));
+        when(authorServiceMock.findAll()).thenReturn(List.of(responseFirst));
 
         ResponseEntity<List<AuthorResponseDto>> responseEntity = authorController.findAll();
 
         assertThat(responseEntity).isNotNull();
         assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
-        assertThat(responseEntity.getBody()).isEqualTo(List.of(responseFirst, responseSecond));
+        assertThat(responseEntity.getBody()).isEqualTo(List.of(responseFirst)); // *#Record assumption#*
         verify(authorServiceMock).findAll();
     }
 
@@ -56,6 +55,123 @@ public class AuthorControllerTest {
         assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
         assertThat(responseEntity.getBody()).isEqualTo(List.of());
         verify(authorServiceMock).findAll();
+    }
+
+    @Test
+    public void find() throws AuthorNotFoundException {
+        when(authorServiceMock.find(ID_TEST_1)).thenReturn(responseFirst);
+
+        ResponseEntity<AuthorResponseDto> responseEntity = authorController.find(ID_TEST_1);
+
+        assertThat(responseEntity).isNotNull();
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(responseEntity.getBody()).isEqualTo(responseFirst); // *#Record assumption#*
+        verify(authorServiceMock).find(ID_TEST_1);
+    }
+
+    @Test
+    public void findThrowsAuthorNotFoundException() throws AuthorNotFoundException {
+        when(authorServiceMock.find(ID_TEST_1)).thenThrow(new AuthorNotFoundException());
+
+        assertThatThrownBy(() ->
+                authorController.find(ID_TEST_1)
+        ).isInstanceOf(
+                AuthorNotFoundException.class
+        );
+
+        verify(authorServiceMock).find(ID_TEST_1);
+    }
+
+    @Test
+    public void create() throws EmailAlreadyExistsException {
+        when(authorServiceMock.create(requestFirst)).thenReturn(responseFirst);
+
+        ResponseEntity<AuthorResponseDto> responseEntity = authorController.create(requestFirst);
+
+        assertThat(responseEntity).isNotNull();
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(responseEntity.getBody()).isEqualTo(responseFirst); // *#Record assumption#*
+        verify(authorServiceMock).create(requestFirst);
+    }
+
+    @Test
+    public void createThrowsEmailAlreadyExists() throws EmailAlreadyExistsException {
+        when(authorServiceMock.create(requestFirst)).thenThrow(new EmailAlreadyExistsException());
+
+        assertThatThrownBy(() ->
+                authorController.create(requestFirst)
+        ).isInstanceOf(
+                EmailAlreadyExistsException.class
+        );
+
+        verify(authorServiceMock).create(requestFirst);
+    }
+
+    @Test
+    public void update() throws AuthorNotFoundException, EmailAlreadyExistsException {
+        when(authorServiceMock.update(ID_TEST_1, requestFirst)).thenReturn(responseFirst);
+
+        ResponseEntity<AuthorResponseDto> responseEntity = authorController.update(ID_TEST_1, requestFirst);
+
+        assertThat(responseEntity).isNotNull();
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(responseEntity.getBody()).isEqualTo(responseFirst); // *#Record assumption#*
+        verify(authorServiceMock).update(ID_TEST_1, requestFirst);
+    }
+
+    @Test
+    public void updateThrowsAuthorNotFound() throws AuthorNotFoundException, EmailAlreadyExistsException {
+        when(authorServiceMock.update(ID_TEST_1, requestFirst)).thenThrow(new AuthorNotFoundException());
+
+        assertThatThrownBy(() ->
+                authorController.update(ID_TEST_1, requestFirst)
+        ).isInstanceOf(
+                AuthorNotFoundException.class
+        );
+
+        verify(authorServiceMock).update(ID_TEST_1, requestFirst);
+    }
+
+    @Test
+    public void updateThrowsEmailAlreadyExists() throws AuthorNotFoundException, EmailAlreadyExistsException {
+        when(authorServiceMock.update(ID_TEST_1, requestFirst)).thenThrow(new EmailAlreadyExistsException());
+
+        assertThatThrownBy(() ->
+                authorController.update(ID_TEST_1, requestFirst)
+        ).isInstanceOf(
+                EmailAlreadyExistsException.class
+        );
+
+        verify(authorServiceMock).update(ID_TEST_1, requestFirst);
+    }
+
+    @Test
+    public void delete() throws AuthorNotFoundException {
+        AuthorDeleteResponseDto responseFirstDelete = new AuthorDeleteResponseDto(
+                ID_TEST_1, NAME_TEST_1, EMAIL_TEST_1, TWO
+        );
+
+        when(authorServiceMock.delete(ID_TEST_1)).thenReturn(responseFirstDelete);
+
+        ResponseEntity<AuthorDeleteResponseDto> responseEntity = authorController.delete(ID_TEST_1);
+
+        assertThat(responseEntity).isNotNull();
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(responseEntity.getBody()).isEqualTo(responseFirstDelete); // *#Record assumption#*
+        verify(authorServiceMock).delete(ID_TEST_1);
+    }
+
+    @Test
+    public void deleteThrowsAuthorNotFound() throws AuthorNotFoundException {
+        when(authorServiceMock.delete(ID_TEST_1)).thenThrow(new AuthorNotFoundException());
+
+        assertThatThrownBy(() ->
+                authorController.delete(ID_TEST_1)
+        ).isInstanceOf(
+                AuthorNotFoundException.class
+        );
+
+        verify(authorServiceMock).delete(ID_TEST_1);
     }
 
 }
